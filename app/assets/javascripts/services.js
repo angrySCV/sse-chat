086bed4569dc3be05fe1af6f61a12429a0a04290
angrySCV <dima@angryscv.ru>
2015-03-15 22:30:07 +0300

    some fix

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -3,7 +3,7 @@
 /** chatModel service, provides chat rooms (could as well be loaded from server) */
 angular.module('sseChat.services', []).service('chatModel', function () {
     var getRooms = function () {
-        return [ {name: 'Room 1', value: 'room1'}, {name: 'Room 2', value: 'room2'},
+        return [ {name: 'Room 1', value: 'room1'}, {name: 'Komnata_sayta', value: 'site_new'},
             {name: 'Room 3', value: 'room3'}, {name: 'Room 4', value: 'room4'},
             {name: 'Room 5', value: 'room5'} ];
     };
